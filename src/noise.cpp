000e35286f533b1a79049ded080da31d9ed191db
Bence Kiglics <bence@kiglics.hu>
2013-08-25 14:46:39 +0200

    Implemented doubleNoise functions

--- src/noise.cpp
+++ src/noise.cpp
@@ -6,7 +6,14 @@
   if (args.Length() <= (I) || !args[I]->IsInt32())                      \
     return ThrowException(Exception::TypeError(                         \
                   String::New("Argument " #I " must be an integer")));  \
-  VAR = args[I]->Int32Value();
+  VAR = args[I]->Int32Value()
+
+#define REQ_DOUBLE_ARG(I, VAR)                                        \
+  double VAR;                                                            \
+  if (args.Length() <= (I) || !args[I]->IsNumber())                   \
+    return ThrowException(Exception::TypeError(                       \
+                  String::New("Argument " #I " must be a number")));  \
+  VAR = args[I]->NumberValue()
 
 using namespace v8;
 using namespace node;
@@ -16,17 +23,29 @@ const int Y_NOISE_GEN = 31337;
 const int Z_NOISE_GEN = 6971;
 const int SEED_NOISE_GEN = 1013;
 
+const int NOISE_MASK = 0x7fffffff;
+
 int noiseImpl(int x, int y, int z, int seed)
 {
 	int n = (
 	    X_NOISE_GEN    * x
 	  + Y_NOISE_GEN    * y
 	  + Z_NOISE_GEN    * z
-	  + SEED_NOISE_GEN * seed) & 0x7fffffff;
+	  + SEED_NOISE_GEN * seed) & NOISE_MASK;
 
    n = (n >> 13) ^ n;
 
-   return (n * (n * n * 60493 + 19990303) + 1376312589) & 0x7fffffff;
+   return (n * (n * n * 60493 + 19990303) + 1376312589) & NOISE_MASK;
+}
+
+double lerp(double a, double b, double w)
+{
+	return (1 - w) * a + w * b;
+}
+
+double smooth(double x)
+{
+	return 3*x*x - 2*x*x*x;
 }
 
 Handle<Value> noise1(const Arguments& args)
@@ -62,6 +81,98 @@ Handle<Value> noise3(const Arguments& args)
 	return scope.Close(Int32::New(noiseImpl(x, y, z, seed)));
 }
 
+Handle<Value> doubleNoise1(const Arguments& args)
+{
+	HandleScope scope;
+
+	REQ_INT_ARG(0, seed);
+	REQ_DOUBLE_ARG(1, x);
+
+	int ix = (int)x;
+	double fx = x - ix;
+
+	double result = lerp(
+		(double)noiseImpl(seed, ix  , 0, 0) / NOISE_MASK,
+		(double)noiseImpl(seed, ix+1, 0, 0) / NOISE_MASK,
+		smooth(fx)
+	);
+
+	return scope.Close(Number::New(result));
+}
+
+Handle<Value> doubleNoise2(const Arguments& args)
+{
+	HandleScope scope;
+
+	REQ_INT_ARG(0, seed);
+	REQ_DOUBLE_ARG(1, x);
+	REQ_DOUBLE_ARG(2, y);
+
+	int ix = (int)x, iy = (int)y;
+	double fx = x - ix, fy = y - iy;
+
+	double result = lerp(
+		lerp(
+			(double)noiseImpl(seed, ix,     iy, 0) / NOISE_MASK,
+			(double)noiseImpl(seed, ix + 1, iy, 0) / NOISE_MASK,
+			smooth(fx)
+		),
+		lerp(
+			(double)noiseImpl(seed, ix,     iy + 1, 0) / NOISE_MASK,
+			(double)noiseImpl(seed, ix + 1, iy + 1, 0) / NOISE_MASK,
+			smooth(fx)
+		),
+		smooth(fy)
+	);
+
+	return scope.Close(Number::New(result));
+}
+
+Handle<Value> doubleNoise3(const Arguments& args)
+{
+	HandleScope scope;
+
+	REQ_INT_ARG(0, seed);
+	REQ_DOUBLE_ARG(1, x);
+	REQ_DOUBLE_ARG(2, y);
+	REQ_DOUBLE_ARG(3, z);
+
+	int ix = (int)x, iy = (int)y, iz = (int)z;
+	double fx = x - ix, fy = y - iy, fz = z - iz;
+
+	double result = lerp(
+		lerp(
+			lerp(
+				(double)noiseImpl(seed, ix,     iy, iz) / NOISE_MASK,
+				(double)noiseImpl(seed, ix + 1, iy, iz) / NOISE_MASK,
+				smooth(fx)
+			),
+			lerp(
+				(double)noiseImpl(seed, ix,     iy + 1, iz) / NOISE_MASK,
+				(double)noiseImpl(seed, ix + 1, iy + 1, iz) / NOISE_MASK,
+				smooth(fx)
+			),
+			smooth(fy)
+		),
+		lerp(
+			lerp(
+				(double)noiseImpl(seed, ix,     iy, iz + 1) / NOISE_MASK,
+				(double)noiseImpl(seed, ix + 1, iy, iz + 1) / NOISE_MASK,
+				smooth(fx)
+			),
+			lerp(
+				(double)noiseImpl(seed, ix,     iy + 1, iz + 1) / NOISE_MASK,
+				(double)noiseImpl(seed, ix + 1, iy + 1, iz + 1) / NOISE_MASK,
+				smooth(fx)
+			),
+			smooth(fy)
+		),
+		smooth(fz)
+	);
+
+	return scope.Close(Number::New(result));
+}
+
 extern "C"
 {
 	void init(Handle<Object> exports)
@@ -69,6 +180,9 @@ extern "C"
 		NODE_SET_METHOD(exports, "noise1", noise1);
 		NODE_SET_METHOD(exports, "noise2", noise2);
 		NODE_SET_METHOD(exports, "noise3", noise3);
+		NODE_SET_METHOD(exports, "doubleNoise1", doubleNoise1);
+		NODE_SET_METHOD(exports, "doubleNoise2", doubleNoise2);
+		NODE_SET_METHOD(exports, "doubleNoise3", doubleNoise3);
 	}
 
 	NODE_MODULE(noise, init)
--- test/test_noise.js
+++ test/test_noise.js
@@ -6,6 +6,9 @@ describe("noise", function() {
         assert.equal(typeof noise.noise1, "function");
         assert.equal(typeof noise.noise2, "function");
         assert.equal(typeof noise.noise3, "function");
+        assert.equal(typeof noise.doubleNoise1, "function");
+        assert.equal(typeof noise.doubleNoise2, "function");
+        assert.equal(typeof noise.doubleNoise3, "function");
     });
 
     describe("#noise1()", function() {
@@ -29,6 +32,126 @@ describe("noise", function() {
             assert.notEqual(noise.noise2(43645, 75857, 69748, 23846), noise.noise1(43645, 75858, 69748, 23846));
             assert.notEqual(noise.noise2(43645, 75857, 69748, 23846), noise.noise1(43645, 75857, 69749, 23846));
             assert.notEqual(noise.noise2(43645, 75857, 69748, 23846), noise.noise1(43645, 75857, 69748, 23847));
+        });
+    });
+
+    describe("#doubleNoise1()", function() {
+        it("should return value between 0.0 and 1.0", function() {
+            var lastValue = null;
+
+            for (var i = 0; i < 10000; i++) {
+                var n = noise.doubleNoise1(12345, i);
+
+                assert(n >= 0, "should be greater than zero");
+                assert(n <= 1, "should be less than 1");
+                assert.notEqual(n, lastValue);
+
+                lastValue = n;
+            }
+        });
+
+        it("should return interpolated value", function() {
+            var n1 = noise.doubleNoise1(12345, 5);
+            var n2 = noise.doubleNoise1(12345, 6);
+            var nIntp = noise.doubleNoise1(12345, 5.5);
+
+            assert.equal(nIntp, (n1 + n2) / 2);
+        });
+
+        it("should not be a linear interpolation", function() {
+            var n1 = noise.doubleNoise1(12345, 5);
+            var n2 = noise.doubleNoise1(12345, 6);
+            var nIntp = noise.doubleNoise1(12345, 5.25);
+
+            assert.notEqual(nIntp, n1 * 0.75 + n2 * 0.25);
+        });
+    });
+
+    describe("#doubleNoise2()", function() {
+        it("should return value between 0.0 and 1.0", function() {
+            var lastValue = null;
+
+            for (var x = 0; x < 100; x++) {
+                for (var y = 0; y < 100; y++) {
+                    var n = noise.doubleNoise2(12345, x, y);
+
+                    assert(n >= 0, "should be greater than zero");
+                    assert(n <= 1, "should be less than 1");
+                    assert.notEqual(n, lastValue);
+
+                    lastValue = n;
+                }
+            }
+        });
+
+        it("should return interpolated value", function() {
+            var n1 = noise.doubleNoise2(12345, 5, 1);
+            var n2 = noise.doubleNoise2(12345, 6, 1);
+            var n3 = noise.doubleNoise2(12345, 5, 2);
+            var nIntp1 = noise.doubleNoise2(12345, 5.5, 1);
+            var nIntp2 = noise.doubleNoise2(12345, 5, 1.5);
+
+            assert.equal(nIntp1, (n1 + n2) / 2);
+            assert.equal(nIntp2, (n1 + n3) / 2);
+        });
+
+        it("should not be a linear interpolation", function() {
+            var n1 = noise.doubleNoise2(12345, 5, 1);
+            var n2 = noise.doubleNoise2(12345, 6, 1);
+            var n3 = noise.doubleNoise2(12345, 5, 2);
+            var nIntp1 = noise.doubleNoise2(12345, 5.25, 1);
+            var nIntp2 = noise.doubleNoise2(12345, 5, 1.25);
+
+            assert.notEqual(nIntp1, n1 * 0.75 + n2 * 0.25);
+            assert.notEqual(nIntp2, n1 * 0.75 + n3 * 0.25);
+        });
+    });
+
+    describe("#doubleNoise3()", function() {
+        it("should return value between 0.0 and 1.0", function() {
+            var lastValue = null;
+
+            for (var x = 0; x < 30; x++) {
+                for (var y = 0; y < 30; y++) {
+                    for (var z = 0; z < 30; z++) {
+                        var n = noise.doubleNoise3(12345, x, y, z);
+
+                        assert(n >= 0, "should be greater than zero");
+                        assert(n <= 1, "should be less than 1 " + n);
+                        assert.notEqual(n, lastValue);
+
+                        lastValue = n;
+                    }
+                }
+            }
+        });
+
+        it("should return interpolated value", function() {
+            var n1 = noise.doubleNoise3(12345, 5, 1, 8);
+            var n2 = noise.doubleNoise3(12345, 6, 1, 8);
+            var n3 = noise.doubleNoise3(12345, 5, 2, 8);
+            var n4 = noise.doubleNoise3(12345, 5, 1, 9);
+            var nIntp1 = noise.doubleNoise3(12345, 5.5, 1, 8);
+            var nIntp2 = noise.doubleNoise3(12345, 5, 1.5, 8);
+            var nIntp3 = noise.doubleNoise3(12345, 5, 1, 8.5);
+
+            assert.equal(nIntp1, (n1 + n2) / 2);
+            assert.equal(nIntp2, (n1 + n3) / 2);
+            assert.equal(nIntp3, (n1 + n4) / 2);
+        });
+
+        it("should not be a linear interpolation", function() {
+            var n1 = noise.doubleNoise3(12345, 5, 1, 8);
+            var n2 = noise.doubleNoise3(12345, 6, 1, 8);
+            var n3 = noise.doubleNoise3(12345, 5, 2, 8);
+            var n4 = noise.doubleNoise3(12345, 5, 1, 9);
+            var nIntp1 = noise.doubleNoise3(12345, 5.25, 1, 8);
+            var nIntp2 = noise.doubleNoise3(12345, 5, 1.25, 8);
+            var nIntp3 = noise.doubleNoise3(12345, 5, 1, 8.25);
+
+            assert.notEqual(nIntp1, n1 * 0.75 + n2 * 0.25);
+            assert.notEqual(nIntp2, n1 * 0.75 + n3 * 0.25);
+            assert.notEqual(nIntp3, n1 * 0.75 + n4 * 0.25);
         });
     });
 });
